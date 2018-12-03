5b6e4a706ba04aa04821dfd2b155d8f5d360d8d5
trialforce@gmail.com <trialforce@gmail.com>
2018-12-03 17:40:20 -0200

    Select: separated method for create option, use in dinamic options construct

--- src/view/option.php
+++ src/view/option.php
@@ -9,16 +9,58 @@ namespace View;
 class Option extends \View\View
 {
 
-    public function __construct( $value, $label, $selected = FALSE, $father = NULL )
+    public function __construct($value = null, $label = NULL, $selected = FALSE, $father = NULL)
     {
-        parent::__construct( 'option', NULL, NULL, NULL, $father );
-        $this->setValue( $value );
-        $this->append( $label );
+        parent::__construct('option', NULL, NULL, NULL, $father);
+        $this->setValue($value);
+        $this->append($label ? $label : $value);
 
-        if ( $selected )
+        if ($selected)
         {
-            $this->setAttribute( 'selected', 'selected' );
+            $this->setAttribute('selected', 'selected');
         }
+    }
+
+    /**
+     * Create an simple option
+     * @param mixed $item anything that is construct an option
+     * @param string $index used when is simple array
+     * @param \View\View $parent parent element, to add direct when create
+     * @return \View\Option the created option
+     */
+    public static function createOption($item, $index = null, $parent = null)
+    {
+        $option = null;
+
+        //allready an option
+        if ($item instanceof \View\Option)
+        {
+            $option = $item;
+
+            if ($parent)
+            {
+                $parent->append($item);
+            }
+        }
+        else if (is_object($item))
+        {
+            //default method for model
+            if (method_exists($item, 'getOptionValue'))
+            {
+                $option = new \View\Option($item->getOptionValue() . '', $item->getOptionLabel() . '', FALSE, $parent);
+            }
+            else //simple object
+            {
+                $values = array_values((array) $item);
+                $option = new \View\Option($values[0], $values[1] . '', FALSE, $parent);
+            }
+        }
+        else //simple array
+        {
+            $option = new \View\Option($index . '', $item . '', FALSE, $parent);
+        }
+
+        return $option;
     }
 
 }
