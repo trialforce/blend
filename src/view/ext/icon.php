d598f3c1c42762bc3fa7f09c3dfd9b4fa0cd2269
janionano <janionano@gmail.com>
2019-12-16 16:39:56 -0300

    added support to brand icons (fab)

--- src/view/ext/icon.php
+++ src/view/ext/icon.php
@@ -18,8 +18,15 @@ class Icon extends \View\I
     {
         //FIXME still needed?
         $icon = str_replace('cancel', 'times', $icon);
+        
+        // as default, the icons are identified by 'fa fa-icone'
+        // if another package is needed, like 'fab', cannot change the name here
+        if (!(stripos($icon, 'fa') === 0))
+        {
+            $icon = 'fa fa-' . $icon;
+        }
 
-        parent::__construct($id, null, 'fa fa-' . $icon);
+        parent::__construct($id, null, $icon);
         $this->addClass($extraClass);
 
         if ($onClick)
