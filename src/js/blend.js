6780227a87fcca76612244a03e7779f9198f463e
Eduardo Bonfandini <trialforce@gmail.com>
2019-12-30 17:24:33 -0300

    Added slider component

--- src/js/blend.js
+++ src/js/blend.js
@@ -37,15 +37,33 @@ window.onload =  function ()
      *
      * @returns {jQuery.fn@call;each}
      */
-    jQuery.fn.play = function () {
-        return this.each(function () {
-            
+    jQuery.fn.play = function () 
+    {
+        return this.each(function () 
+        {
             if (typeof this.play === 'function')
             {
                 this.play();
             }
         });
     };
+    
+    //jquery plugin to create element
+    //https://github.com/ern0/jquery.create/blob/ster/jquery.create.js
+    (function($) 
+    {
+        $.create = function(tag,id) 
+        {
+            let elm = document.createElement(tag.toUpperCase());
+
+            if (typeof(id) != "undefined") 
+            {
+                elm.id = id;
+            }
+
+            return $(elm);
+        }; // $.create()
+    }(jQuery));
     
     //destroy popup on esc
     document.keyup = function (e)
@@ -113,12 +131,14 @@ function dataAjax()
                     toast('Ação desabilitada!');
                     return false;
                 });
-            } else if (dataAjax === 'noFormData')
+            }
+            else if (dataAjax === 'noFormData')
             {
                 element.click(function () {
                     return g(href, '');
                 });
-            } else
+            } 
+            else
             {
                 element.click(function () {
                     return p(href);
@@ -391,6 +411,12 @@ function dataAjax()
     {
         $('body').removeClass('os-android').addClass('os-android');
     }
+    
+    //blend slider
+    $('.slider').each(function ()
+    {
+        slide('#' + $(this).attr('id'))
+    });
     
     actionList.restore();
     grid.restoreTextSize();    
@@ -1837,23 +1863,6 @@ function sortList(ul)
     });
 }
 
-//jquery plugin to create element
-//https://github.com/ern0/jquery.create/blob/ster/jquery.create.js
-(function($) 
-{
-    $.create = function(tag,id) 
-    {
-        let elm = document.createElement(tag.toUpperCase());
-        
-        if (typeof(id) != "undefined") 
-        {
-            elm.id = id;
-        }
-        
-        return $(elm);
-    }; // $.create()
-}(jQuery));
-
 var grid = {};
 
 grid.changeTextSize = function(element)
@@ -2020,3 +2029,232 @@ function printScreenFinalize()
         $('#divLegal').html(printScreenBackup);
     }
 }
+
+/**
+ * Scroll to top
+ * @returns void
+ */
+function scrollTop()
+{
+    $("html, body").animate({ scrollTop: 0 }, 300);
+}
+
+/**
+ * Create a simple slider, with mobile support
+ * @param string selector the jquery selector
+ * @returns void
+ */
+function slide(selector)
+{
+    var wrapper = $(selector).get(0);
+    var items = $(wrapper).find('.slider-items').get(0);
+    var prev = $(wrapper).find('.slider-prev').get(0);
+    var next = $(wrapper).find('.slider-next').get(0);
+
+    //copy outter width to inner
+    var outterWidth = $(wrapper).css('width');
+    var outterHeight = $(wrapper).css('height');
+    
+    //don't proccess the same slide again
+    if ($(wrapper).hasClass('loaded'))
+    {
+        return;
+    }
+
+    //if the height it not loaded yet, wait a little
+    if (outterHeight == 0 || outterHeight == '0px')
+    {
+        setTimeout(function ()
+        {
+            slide(selector)
+        }, 100);
+        
+        return;
+    }
+    
+    //if it don't has any slide, does nothing
+    var slideCount = $(wrapper).find('.slide').length;
+    
+    if (slideCount == 0 )
+    {
+        $(wrapper).find('.slider-prev').remove();
+        $(wrapper).find('.slider-next').remove();
+        return;
+    }
+
+    $(wrapper).find('.slide').css('width', outterWidth);
+    $(wrapper).find('.slide').css('height', outterHeight);
+    $(wrapper).find('.slider-items').css('left', '-' + outterWidth);
+    $(wrapper).find('.slider-wrapper').css('height', outterHeight);
+
+    var posX1 = 0;
+    var posX2 = 0;
+    var posInitial = 0;
+
+    var posInitialY = 0;
+    var posY1 = 0;
+    var posY2 = 0;
+
+    var posFinal;
+    var index = 0;
+    var threshold = 30;
+    var allowShift = true;
+
+    var slides = items.getElementsByClassName('slide');
+    var slidesLength = slides.length;
+    var slideSize = items.getElementsByClassName('slide')[0].offsetWidth;
+
+    var firstSlide = slides[0];
+    var lastSlide = slides[slidesLength - 1];
+
+    var cloneFirst = firstSlide.cloneNode(true);
+    var cloneLast = lastSlide.cloneNode(true);
+
+    // Clone first and last slide
+    items.appendChild(cloneFirst);
+    items.insertBefore(cloneLast, firstSlide);
+    wrapper.classList.add('loaded');
+
+    // Mouse and Touch events
+    items.onmousedown = dragStart;
+
+    // Touch events
+    items.addEventListener('touchstart', dragStart, {passive: true});
+    items.addEventListener('touchend', dragEnd, {passive: true});
+    items.addEventListener('touchmove', dragAction, {passive: true});
+
+    // Click events
+    if (prev)
+    {
+        prev.addEventListener('click', function ()
+        {
+            shiftSlide(-1);
+        }, {passive: true});
+    }
+
+    if (next)
+    {
+        next.addEventListener('click', function ()
+        {
+            shiftSlide(1);
+        }, {passive: true});
+    }
+
+    // Transition events
+    items.addEventListener('transitionend', checkIndex, true);
+
+    function dragStart(e)
+    {
+        e = e || window.event;
+        //e.preventDefault();
+        posInitial = items.offsetLeft;
+        posInitialY = $(window).scrollTop();
+
+        if (e.type == 'touchstart')
+        {
+            posX1 = e.touches[0].clientX;
+            posY1 = e.touches[0].clientY;
+        } 
+        else
+        {
+            posX1 = e.clientX;
+            posY1 = e.clientY;
+            document.onmouseup = dragEnd;
+            document.onmousemove = dragAction;
+        }
+    }
+
+    function dragAction(e)
+    {
+        e = e || window.event;
+
+        if (e.type == 'touchmove')
+        {
+            posX2 = posX1 - e.touches[0].clientX;
+            posX1 = e.touches[0].clientX;
+
+            posY2 = posY1 - e.touches[0].clientY;
+        } 
+        else
+        {
+            posX2 = posX1 - e.clientX;
+            posX1 = e.clientX;
+
+            posY2 = posY1 - e.clientY;
+            //posY1 = e.clientY;
+        }
+
+        items.style.left = (items.offsetLeft - posX2) + "px";
+
+        $(window).scrollTop(posInitialY + posY2);
+    }
+
+    function dragEnd(e)
+    {
+        posFinal = items.offsetLeft;
+
+        if (posFinal - posInitial < -threshold)
+        {
+            shiftSlide(1, 'drag');
+        } 
+        else if (posFinal - posInitial > threshold)
+        {
+            shiftSlide(-1, 'drag');
+        }
+        else
+        {
+            items.style.left = (posInitial) + "px";
+        }
+
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+
+    function shiftSlide(dir, action)
+    {
+        items.classList.add('shifting');
+
+        if (allowShift)
+        {
+            if (!action)
+            {
+                posInitial = items.offsetLeft;
+            }
+
+            if (dir == 1)
+            {
+                items.style.left = (posInitial - slideSize) + "px";
+                index++;
+            } 
+            else if (dir == -1)
+            {
+                items.style.left = (posInitial + slideSize) + "px";
+                index--;
+            }
+        };
+
+        allowShift = false;
+
+        //event.preventDefault();
+        return false;
+    }
+
+    function checkIndex()
+    {
+        items.classList.remove('shifting');
+
+        if (index == -1)
+        {
+            items.style.left = -(slidesLength * slideSize) + "px";
+            index = slidesLength - 1;
+        }
+
+        if (index == slidesLength)
+        {
+            items.style.left = -(1 * slideSize) + "px";
+            index = 0;
+        }
+
+        allowShift = true;
+    }
+};
---
+++ src/view/ext/slider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace View\Ext;
+
+class Slider extends \View\Div
+{
+
+    protected $items;
+
+    public function __construct($id = \NULL, $extraClass = '')
+    {
+
+        $this->items = new \View\Div(null, null, 'slider-items');
+        $wrapper = new \View\Div(null, null, 'slider-wrapper');
+        $wrapper->append($this->items);
+
+        parent::__construct($id, $wrapper, 'slider');
+        $this->addClass($extraClass);
+    }
+
+    public function addNextPrevControl()
+    {
+        $controls[] = new \View\Div(null, null, 'slider-control slider-prev');
+        $controls[] = new \View\Div(null, null, 'slider-control slider-next');
+        $this->append($controls);
+
+        return $this;
+    }
+
+    public function addSlide(\View\View $view)
+    {
+        $view->addClass('slide');
+        $this->items->append($view);
+    }
+
+}
+
+/*<div id="slider2" class="slider" style="width: 300px;">
+        <div class="slider-wrapper">
+            <div class="slider-items">
+                <span class="slide" style="background: #7ADCEF;">Other 1</span>
+                <span class="slide" style="background: #FFCF47;">Other 2</span>
+                <span class="slide" style="background: #F97C68;">Other 3</span>
+                <span class="slide" style="background: #a78df5;">Other 4</span>
+                <span class="slide" style="background: #ff8686;">Other 5</span>
+            </div>
+        </div>
+        <div class="slider-control slider-prev"></div>
+        <div class="slider-control slider-next"></div>
+    </div>*/
