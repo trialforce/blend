de19c3322d9b70c0292acafe1fb60be10ff21519
Eduardo Bonfandini <trialforce@gmail.com>
2020-03-31 20:22:22 -0300

    Litte improvment in badge with relation with \Db\ConstantValue

--- src/view/ext/badge.php
+++ src/view/ext/badge.php
@@ -11,6 +11,7 @@ class Badge extends \View\Span
 {
 
     /**
+     * Construct a Badge
      *
      * @param string $id the attr id from html
      * @param mixed $innerHtml content of the badge
@@ -19,6 +20,26 @@ class Badge extends \View\Span
     public function __construct($id = \NULL, $innerHtml = \NULL, $class = \NULL)
     {
         parent::__construct($id, $innerHtml, 'badge ' . $class);
+    }
+
+    /**
+     * Create a color bagde based on a \Db\ConstantValues and a selected values
+     *
+     * @param \Db\ConstantValues $cValues the constant values
+     * @param string $value the selected value
+     * @return \View\Ext\Badge the created badge
+     */
+    public static function createFromConstantValues(\Db\ConstantValues $cValues, $value)
+    {
+        $vector = $cValues->getArray();
+
+        $class = get_class($cValues);
+        $colors = $class::getColors();
+
+        $badge = new \View\Ext\Badge(null, $vector[$value]);
+        $badge->css('background-color', $colors[$value]);
+
+        return $badge;
     }
 
 }
