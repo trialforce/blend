10ac3b70dcc05a3d788c2851628dd077d7200247
janionano <janionano@gmail.com>
2020-05-12 20:04:49 -0300

    added function slug to optimize a string to a url

--- src/js/blend.js
+++ src/js/blend.js
@@ -2357,3 +2357,29 @@ function scrollTop()
 {
     $("html, body").animate({ scrollTop: 0 }, 300);
 }
+
+/**
+ * Turn string into a url optimized string easily.
+ * @param string str
+ * @returns url optimized string
+ */
+function slug(str) 
+{
+    str = str.replace(/^\s+|\s+$/g, ''); // strong trim
+    str = str.toLowerCase();
+
+    // remove accents, swap ñ for n, etc
+    var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+    var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+
+    for (var i=0, l=from.length ; i<l ; i++) 
+    {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+}
