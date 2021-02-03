dbc35c17a2c32f186b40c062fecbe3dd31630379
Eduardo Bonfandini <trialforce@gmail.com>
2021-02-03 13:45:14 -0300

    Little correction in fixed filter

--- src/page/crud.php
+++ src/page/crud.php
@@ -763,6 +763,7 @@ class Crud extends \Page\Page
         $collection = new \Db\Collection($options);
         $filter = new \Filter\Collection($column, $collection);
         $filter->setDefaultValue($defaultValue);
+        $filter->setFilterType(\Filter\Text::FILTER_TYPE_ENABLE_SHOW_ALWAYS);
 
         if (!$onlyFilter)
         {
