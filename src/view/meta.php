661a711eddd576580cc6536c7b865ce5c18cf30b
Eduardo Bonfandini <trialforce@gmail.com>
2021-06-10 11:10:32 -0300

    Improving meta html object

--- src/view/meta.php
+++ src/view/meta.php
@@ -1,16 +1,31 @@
 <?php
+
 namespace View;
+
 /**
  * Html meta tag
  */
 class Meta extends \View\View
 {
 
-    public function __construct( $name = NULL, $innerHtml = NULL )
+    public function __construct($name = NULL, $innerHtml = NULL, $content = null)
     {
-        parent::__construct( 'meta' );
-        $this->setAttribute( 'name', $name );
-        $this->append( $innerHtml );
+        parent::__construct('meta');
+        $this->setAttribute('name', $name);
+        $this->append($innerHtml);
+        $this->setContent($content);
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content content
+     * @return $this
+     *
+     */
+    public function setContent($content)
+    {
+        return $this->setAttribute('content', $content);
     }
 
 }
