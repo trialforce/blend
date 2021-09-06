49c388b71d899c55f1afc2fbb4e7a1569d02d1b4
Eduardo Bonfandini <trialforce@gmail.com>
2021-09-06 11:40:58 -0300

    Improved lazyloading (onscrollDow onScrollUp)

--- src/js/plugin/blend.lazyloading.js
+++ src/js/plugin/blend.lazyloading.js
@@ -12,63 +12,63 @@
  */
 
 blend.lazyloading = {};
+//default image offset adjust
 blend.lazyloading.adjust = 40;
-blend.plugins.push(blend.lazyloading); 
+//current height visible
+blend.lazyloading.heightVisible = 0;
+blend.lazyloading.lastHeightVisible = 0;
+//scroll events/methods
+blend.lazyloading.onScrollUp = null;
+blend.lazyloading.onScrollDown = null;
+blend.plugins.push(blend.lazyloading);
 
-blend.lazyloading.register = function()
+blend.lazyloading.register = function ()
 {
-    window.addEventListener("scroll", blend.lazyloading.onScroll,{passive:true});
+    window.addEventListener("scroll", blend.lazyloading.onScroll, {passive: true});
     blend.lazyloading.parse();
 };
 
-blend.lazyloading.start = function()
-{
-    blend.lazyloading.parse();
-}; 
-
-blend.lazyloading.onScroll = function()
+blend.lazyloading.start = function ()
 {
     blend.lazyloading.parse();
 };
 
-blend.lazyloading.parse = function()
+blend.lazyloading.onScroll = function ()
+{
+    blend.lazyloading.parse();
+};
+
+blend.lazyloading.parseBackImages = function ()
 {
     var elements = $('[data-lazyloading-background-image]');
-    var imgs = $('[data-lazyloading-src]');
-    var actives = $('[data-lazyloading-active]');
-    var functions = $('[data-lazyloading-function]');
-    
-    var scrollingElement = document.documentElement;
-    
-    if (document.scrollingElement)
-    {
-        scrollingElement = document.scrollingElement;
-    }
-    
-    var heightVisible = scrollingElement.scrollTop+screen.height;
-    
+
     //background image
-    elements.each(function(idx)
+    elements.each(function (idx)
     {
         var element = $(elements[idx]);
         var offsetTop = element.offset().top - blend.lazyloading.adjust;
-    
-        if ( heightVisible > offsetTop)
+
+        if (blend.lazyloading.heightVisible > offsetTop)
         {
             var image = element.data('lazyloading-background-image');
-            element.css('background-image', 'url('+image+')');
+            element.css('background-image', 'url(' + image + ')');
             element.removeData('lazyloading-background-image');
             element.removeAttr('data-lazyloading-background-image');
         }
     });
-    
+}
+
+blend.lazyloading.parseSrcImages = function ()
+{
+    var imgs = $('[data-lazyloading-src]');
+
     //images with href
-    imgs.each(function(idx)
+    imgs.each(function (idx)
     {
         var element = $(imgs[idx]);
         var offsetTop = element.offset().top - blend.lazyloading.adjust;
-    
-        if ( heightVisible > offsetTop)
+
+        if (blend.lazyloading.heightVisible > offsetTop)
         {
             var image = element.data('lazyloading-src');
             element.attr('src', image);
@@ -76,45 +76,88 @@ blend.lazyloading.parse = function()
             element.removeAttr('data-lazyloading-src');
         }
     });
-    
+}
+
+blend.lazyloading.parseActives = function ()
+{
+    var actives = $('[data-lazyloading-active]');
+
     //element to active (add class lazyloading-active)
-    actives.each(function(idx) 
+    actives.each(function (idx)
     {
         var element = $(actives[idx]);
         var adjust = 150;
         var offsetTop = element.offset().top + (adjust);
-          
-        if ( heightVisible > offsetTop)
+
+        if (blend.lazyloading.heightVisible > offsetTop)
         {
             element.addClass('lazyloading-active');
             element.removeData('lazyloading-active');
             element.removeAttr('data-lazyloading-active');
         }
     });
-    
+}
+
+blend.lazyloading.parseFunctions = function ()
+{
+    var functions = $('[data-lazyloading-function]');
+
     //elements to call function
-    functions.each(function(idx)
+    functions.each(function (idx)
     {
         var element = $(functions[idx]);
         var adjust = 150;
         var offsetTop = element.offset().top + (adjust);
-          
-        if ( heightVisible > offsetTop)
+
+        if (blend.lazyloading.heightVisible > offsetTop)
         {
             var method = element.data('lazyloading-function');
-            
+
             if (typeof method == 'function')
             {
                 method();
-            }
+            } 
             else
             {
                 eval(method);
             }
-            
+
             element.addClass('lazyloading-function');
             element.removeData('lazyloading-function');
             element.removeAttr('data-lazyloading-function');
         }
     });
+}
+
+blend.lazyloading.parse = function ()
+{
+    var scrollingElement = document.documentElement;
+
+    if (document.scrollingElement)
+    {
+        scrollingElement = document.scrollingElement;
+    }
+
+    blend.lazyloading.heightVisible = scrollingElement.scrollTop + screen.height;
+    blend.lazyloading.parseBackImages();
+    blend.lazyloading.parseSrcImages();
+    blend.lazyloading.parseActives();
+    blend.lazyloading.parseFunctions();
+
+    if (blend.lazyloading.heightVisible > blend.lazyloading.lastHeightVisible)
+    {
+        if (typeof blend.lazyloading.onScrollDown == 'function')
+        {
+            blend.lazyloading.onScrollDown();
+        }
+    } 
+    else if (blend.lazyloading.heightVisible < blend.lazyloading.lastHeightVisible)
+    {
+        if (typeof blend.lazyloading.onScrollUp == 'function')
+        {
+            blend.lazyloading.onScrollUp();
+        }
+    }
+
+    blend.lazyloading.lastHeightVisible = blend.lazyloading.heightVisible;
 };
