351d41b92ae68e9413ebe912d4a2fccaf5181843
Eduardo Bonfandini <trialforce@gmail.com>
2022-01-24 15:05:33 -0300

    sds

---
+++ src/view/ext/notification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace View\Ext;
+
+/**
+ * Simple JS notification
+ */
+Class Notification
+{
+
+    protected $title;
+    protected $body;
+    protected $link;
+    protected $icon;
+
+    public function __construct($title, $body, $link = null, $icon = null)
+    {
+        $this->title = $title;
+        $this->body = $body;
+        $this->link = $link;
+        $this->icon = $icon;
+
+        \View\View::getDom()->append($this);
+    }
+
+    public function __toString()
+    {
+        $this->body = strip_tags($this->body);
+        $js = "<script>blend.notification.new('{$this->title}', '$this->body', '{$this->link}','{$this->icon}');</script>";
+        return $js;
+    }
+
+}
