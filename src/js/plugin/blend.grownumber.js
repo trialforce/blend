d04eaa04e2f9c0e9daff8271e9d5a2ccd6b78d3f
Eduardo Bonfandini <trialforce@gmail.com>
2022-02-18 14:45:31 -0300

    Corrected grow number

--- src/js/plugin/blend.grownumber.js
+++ src/js/plugin/blend.grownumber.js
@@ -7,48 +7,52 @@
 
 blend.grownumber = {};
 blend.grownumber.defaultTime = 1000; //ms
-blend.plugins.push(blend.grownumber); 
+blend.plugins.push(blend.grownumber);
 
-blend.grownumber.register = function()
+blend.grownumber.register = function ()
 {
 };
 
-blend.grownumber.start = function()
+blend.grownumber.start = function ()
 {
     var elements = $('[data-grow-number]');
-    
-    elements.each (function(idx)
+
+    elements.each(function (idx)
     {
         var element = $(elements[idx]);
         var valueOriginal = toNumber(element.data('grow-number'));
         var intervalTime = blend.grownumber.defaultTime / valueOriginal;
         var increment = 1;
-        
+
         if (intervalTime < 10)
         {
             increment = 10;
             intervalTime *= 10;
         }
-        
+
         element.html('0');
-        
-        var myInterval = setInterval( function()
+
+        var myInterval = setInterval(function ()
         {
-            var newValue = parseInt(element.text())+increment;
+            var newValue = parseInt(element.text()) + increment;
+
+            if (newValue > valueOriginal)
+            {
+                newValue = valueOriginal;
+            }
+
             if (newValue < valueOriginal)
             {
                 element.html(newValue);
-            }
-            
-            if (newValue == valueOriginal)
+            } else
             {
                 element.html(newValue);
                 clearInterval(myInterval);
                 //avoid do ite again
                 element.removeAttr('data-grow-number');
             }
-            
+
         }, intervalTime);
-        
+
     });
 }; 
