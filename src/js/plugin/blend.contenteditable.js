bc4861c583ccf716b00a98af719b0730fc2d7e82
Eduardo Bonfandini <trialforce@gmail.com>
2022-04-20 15:24:16 -0300

    ContentEditable, not finished, but working

--- src/js/plugin/blend.contenteditable.js
+++ src/js/plugin/blend.contenteditable.js
@@ -4,9 +4,32 @@ blend.plugins.push(blend.contentEditable);
 blend.contentEditable.cssClass= 'content-editable';
 blend.contentEditable.menu = {};
 blend.contentEditable.menu.buttonCssClass= 'content-editable-button';
+blend.contentEditable.menu.selectCssClass= 'content-editable-select';
+blend.contentEditable.menu.colorCssClass= 'content-editable-color';
 blend.contentEditable.menu.cssClass= 'content-editable-menu';
-blend.contentEditable.buttons = {};
+//blend.contentEditable.buttons = {};
 blend.contentEditable.actions = {};
+
+blend.contentEditable.availableFonts = [
+        {"value":"","label":'Sem fonte'},
+        {"value":"Arial","label":'Arial'},
+        {"value":"Arial Black","label":'Arial Black'},
+        {"value":"Courier New","label":'Courier New'},
+        {"value":"Times New Roman","label":'Times New Roman'}
+    ];
+    
+blend.contentEditable.availableBlocks = [
+        {"value":"","label":'Sem formatação'},
+        {"value":"h1","label":'Título principal'},
+        {"value":"h2","label":'Título 2'},
+        {"value":"h3","label":'Título 3'},
+        {"value":"h4","label":'Título 4'},
+        {"value":"h5","label":'Título 5'},
+        {"value":"h6","label":'Título 6'},
+        {"value":"pre","label":'Pre'},
+        {"value":"p","label":'Parágrafo'},
+        {"value":"blockquote","label":'Citação'}
+    ];
 
 blend.contentEditable.getNodeList = function()
 {
@@ -41,11 +64,11 @@ blend.contentEditable.start = function()
             continue;
         }
         
-        //var menu = blend.contentEditable.createMenu(element);
-        //parent.appendChild(menu);
-        //console.log(parent);
+        var menu = blend.contentEditable.createMenu(element);
+        parent.appendChild(menu);
     }
 };
+
 
 blend.contentEditable.createMenu = function( element )
 {
@@ -53,12 +76,42 @@ blend.contentEditable.createMenu = function( element )
     menu.classList.add(blend.contentEditable.menu.cssClass);
     //menu.innerHTML = 'menu';
     menu.appendChild( blend.contentEditable.createButton(element,'broom', 'clear','Limpar'));
+    menu.appendChild( blend.contentEditable.createButton(element,'paint-brush', 'removeFormat','Remover formatação'));
     menu.appendChild( blend.contentEditable.createButton(element,'undo', 'undo','Desfazer'));
     menu.appendChild( blend.contentEditable.createButton(element,'redo', 'redo','Refazer'));
-    menu.appendChild( blend.contentEditable.createButton(element,'remove-formaT', 'removeformat','Remover formatação'));
     menu.appendChild( blend.contentEditable.createButton(element,'bold', 'bold','Negrito'));
     menu.appendChild( blend.contentEditable.createButton(element,'italic', 'italic','Itálico'));
     menu.appendChild( blend.contentEditable.createButton(element,'underline', 'underline','Underline'));
+    menu.appendChild( blend.contentEditable.createButton(element,'link', 'createLink','Criar link'));
+    
+    menu.appendChild( blend.contentEditable.createButton(element,'align-left', 'alignLeft','Alinhar esquerda'));
+    menu.appendChild( blend.contentEditable.createButton(element,'align-center', 'alignCenter','Centralizado'));
+    menu.appendChild( blend.contentEditable.createButton(element,'align-right', 'alignRight','Alinhr direita'));
+    
+    menu.appendChild( blend.contentEditable.createButton(element,'copy', 'copy','Copiar'));
+    menu.appendChild( blend.contentEditable.createButton(element,'cut', 'cut','Recortar'));
+    menu.appendChild( blend.contentEditable.createButton(element,'paste', 'paste','Colar'));
+    
+    menu.appendChild( blend.contentEditable.createButton(element,'list-ul', 'insertUnorderedList','Lista'));
+    menu.appendChild( blend.contentEditable.createButton(element,'list-ol', 'insertOrderedList','Lista numérica'));
+    
+    menu.appendChild( blend.contentEditable.createButton(element,'long-arrow-alt-right', 'indent','Identar'));
+    menu.appendChild( blend.contentEditable.createButton(element,'long-arrow-alt-left', 'outdent','Unidentar'));
+    
+    menu.appendChild( blend.contentEditable.createOptions(element,blend.contentEditable.availableBlocks, 'formatBlock','Elemento'));
+    menu.appendChild( blend.contentEditable.createOptions(element,blend.contentEditable.availableFonts, 'fontName','Fonte'));
+    
+    var sizes = [];
+    
+    for (var i=1; i < 10; i++)
+    {
+        sizes.push( {"value":i,"label":i+'pt'} );
+    }
+        
+    menu.appendChild( blend.contentEditable.createOptions(element,sizes, 'fontSize','Tamanho'));
+    
+    menu.appendChild( blend.contentEditable.createColorButton(element, 'foreColor','Tamanho'));
+    menu.appendChild( blend.contentEditable.createColorButton(element, 'backColor','Tamanho'));
     
     return menu;
 };
@@ -66,80 +119,210 @@ blend.contentEditable.createMenu = function( element )
 blend.contentEditable.createButton = function(element, icon, action, title)
 {
     var button = document.createElement('div');
-    button.classList.add(blend.contentEditable.menu.buttonCssClass);
-    button.setAttribute('onclick','blend.contentEditable.actions.'+action+'(\'#'+element.id+'\')');
-    button.innerHTML = '<i class="fa fa-'+icon+'"></i>';
     button.title= title;
+    
+    button.classList.add(blend.contentEditable.menu.buttonCssClass);
+    button.classList.add('fa');
+    button.classList.add('fa-'+icon);
+    button.setAttribute('onmousedown','event.preventDefault();');
+    button.setAttribute('onclick','blend.contentEditable.actions.'+action+'(\'#'+element.id+'\')');
+    
     return button;
 };
 
+blend.contentEditable.createOptions = function(element, options, action, title)
+{
+    //create "null element
+    if ( options.length == 0)
+    {
+        var element = document.createElement('div');
+        element.style.display= "none";
+        return element;
+    }
+    
+    var select = document.createElement('select');
+    select.title= title;
+    select.classList.add(blend.contentEditable.menu.selectCssClass);
+    select.setAttribute('onchange','blend.contentEditable.actions.'+action+'(\'#'+element.id+'\',this)');
+    
+    for (var i=0; i<options.length; i++)
+    {
+        var option = options[i];
+        var opt = document.createElement('option');
+        opt.value = option.value;
+        opt.innerHTML = option.label;
+        select.appendChild(opt);
+    }
+    
+    return select;
+}
+
+blend.contentEditable.createColorButton = function(element, action, title)
+{
+    var button = document.createElement('input');
+    button.type= 'color';
+    button.title= title;
+    
+    button.classList.add(blend.contentEditable.menu.colorCssClass);
+    button.setAttribute('onchange','blend.contentEditable.actions.'+action+'(\'#'+element.id+'\',this)');
+    
+    return button;
+};
 
 blend.contentEditable.actions.clear = function(selector)
 {
     var element = document.querySelector(selector);
     var editable = element.querySelector('[contenteditable]');
     editable.innerHTML = '';
-    element.focus();
-}
+};
 
 blend.contentEditable.actions.undo = function(selector)
 {
-    console.log(selector);
-    var editable = document.querySelector(selector+ ' [contenteditable]');
     document.execCommand('undo', false, null);
-    editable.focus();
-}
+};
 
 blend.contentEditable.actions.redo = function(selector)
 {
-    var editable = document.querySelector(selector+ ' [contenteditable]');
     document.execCommand('redo', false, null);
-    editable.focus();
-}
+};
 
 blend.contentEditable.actions.removeFormat = function(selector)
 {
-    var editable = document.querySelector(selector+ ' [contenteditable]');
     document.execCommand('removeFormat', false, null);
-    editable.focus();
-}
+};
 
 blend.contentEditable.actions.bold = function(selector)
 {
-    console.log('bold');
-    var editable = document.querySelector(selector+ ' [contenteditable]');
     document.execCommand('bold', false, null);
-    editable.focus();
-}
+};
 
 blend.contentEditable.actions.italic = function(selector)
 {
-    var element = document.querySelector(selector);
     document.execCommand('italic', false, null);
-    element.focus();
-}
+};
 
 blend.contentEditable.actions.underline = function(selector)
 {
-    var element = document.querySelector(selector);
     document.execCommand('underline', false, null);
-    element.focus();
+};
+
+blend.contentEditable.actions.alignLeft = function(selector)
+{
+    document.execCommand('justifyleft', false, null);
+};
+
+blend.contentEditable.actions.alignCenter = function(selector)
+{
+    document.execCommand('justifycenter', false, null);
+};
+
+blend.contentEditable.actions.alignRight = function(selector)
+{
+    document.execCommand('justifyright', false, null);
+};
+
+blend.contentEditable.actions.copy = function(selector)
+{
+    document.execCommand('copy', false, null);
+};
+
+blend.contentEditable.actions.cut = function(selector)
+{
+    document.execCommand('cut', false, null);
+};
+
+blend.contentEditable.actions.paste = function(selector)
+{
+    document.execCommand('paste', false, null);
+};
+
+blend.contentEditable.actions.formatBlock = function(selector, element)
+{
+    var tag = element[element.selectedIndex].value;
+   
+    if ( tag )
+    {
+        document.execCommand('formatBlock', false, tag);
+    }
+    else
+    {
+        const selection = window.getSelection()
+        
+        if (!selection.isCollapsed)
+        {
+            selection.anchorNode.parentNode.replaceWith(selection.anchorNode)
+        } 
+    }
+};
+
+blend.contentEditable.actions.fontName = function(selector, element)
+{
+    var font = element[element.selectedIndex].value;
+   
+    if ( font )
+    {
+        document.execCommand('fontname', false, font);
+    }
+    else
+    {
+        const selection = window.getSelection()
+        
+        if (!selection.isCollapsed)
+        {
+            selection.anchorNode.parentNode.replaceWith(selection.anchorNode)
+        } 
+    }
+};
+
+blend.contentEditable.actions.fontSize = function(selector, element)
+{
+    var size = element[element.selectedIndex].value;
+   
+    document.execCommand('fontsize', false, size);    
+};
+
+blend.contentEditable.actions.foreColor = function(selector, element)
+{
+    var color = element.value;
+    document.execCommand('foreColor', false, color);
+};
+
+blend.contentEditable.actions.backColor = function(selector, element)
+{
+    var color = element.value;
+    document.execCommand('backColor', false, color);
+};
+
+blend.contentEditable.actions.insertOrderedList = function(selector, element)
+{
+    console.log('order');
+    document.execCommand('insertorderedlist', false, null);
+};
+
+blend.contentEditable.actions.insertUnorderedList = function(selector, element)
+{
+    console.log('unorder');
+    document.execCommand('insertunorderedlist', false, null);
+};
+
+blend.contentEditable.actions.outdent = function(selector, element)
+{
+    document.execCommand('outdent', false, null);
+};
+
+blend.contentEditable.actions.indent = function(selector, element)
+{
+    document.execCommand('indent', false, null);
+};
+
+blend.contentEditable.actions.createLink = function(selector, element)
+{
+    var sLnk = prompt('Link', 'http:\/\/');
+    
+    if (sLnk && sLnk != '' && sLnk != 'http://')
+    {
+        document.execCommand('createlink', false, sLnk)
+    }
 }
 
-//copy
-//cut
-//paste
-//justifycenter;
-//justifyright
-//insertorderedlist
-//insertunorderedlist
-//outdent
-//indent
-//formatblock blockquote, h1 .. p, pre
 //createlink $link
-//fontname $fontname
-//fontsize $size
-//forecolor $color
-//backcolor $color
-
-//blend.contentEditable.buttons.push();
