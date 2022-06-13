2640d73abe42544f5107c4375cd87f3818b9a33f
Darlan Ullmann <darlanullmann@gmail.com>
2022-06-13 17:21:10 -0300

    Alterado cookie de sessão
    Alterado cookie de sessão para SameSite=Lax, para resolver problema de click de sites externos para a aplicação

--- src/blend.php
+++ src/blend.php
@@ -14,7 +14,7 @@ if (session_status() == PHP_SESSION_NONE)
     ini_set('session.use_only_cookies', 1);
     ini_set('session.use_strict_mode', 1);
     ini_set('session.cookie_httponly', 1);
-    ini_set('session.cookie_samesite', 'Strict');
+    ini_set('session.cookie_samesite', 'Lax');
 
     if (\DataHandle\Server::getInstance()->isHttps())
     {
