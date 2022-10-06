1f4a762158591e4c9d94673173872c568cb59c6c
Eduardo Bonfandini <trialforce@gmail.com>
2022-10-06 10:46:57 -0300

    Improving setValue of float and int input

--- src/view/ext/intinput.php
+++ src/view/ext/intinput.php
@@ -9,22 +9,34 @@ namespace View\Ext;
 class IntInput extends \View\Input
 {
 
-    public function __construct( $idName, $value = NULL, $vMax = 99999999999999, $vMin = -99999999999, $class = NULL )
+    public function __construct($idName, $value = NULL, $vMax = 99999999999999, $vMin = -99999999999, $class = NULL)
     {
-        parent::__construct( $idName, \View\Input::TYPE_NUMBER, $value, $class );
+        parent::__construct($idName, \View\Input::TYPE_NUMBER, $value, $class);
 
         $vMax = $vMax ? $vMax : 99999999999999;
         $vMin = $vMin ? $vMin : -99999999999;
 
-        $this->setAttribute( 'keyboard', 'numeric' );
+        $this->setAttribute('keyboard', 'numeric');
 
-        $this->setAttribute( 'data-m-dec', 0 );
-        $this->setAttribute( 'data-a-sep', '' );
-        $this->setAttribute( 'data-a-dec', '.' );
-        $this->setAttribute( 'max', $vMax );
-        $this->setAttribute( 'min', $vMin );
+        $this->setAttribute('data-m-dec', 0);
+        $this->setAttribute('data-a-sep', '');
+        $this->setAttribute('data-a-dec', '.');
+        $this->setAttribute('max', $vMax);
+        $this->setAttribute('min', $vMin);
 
-        $this->addClass( 'integer' );
+        $this->addClass('integer');
+    }
+
+    public function setValue($value)
+    {
+        if ($value instanceof \Type\Generic)
+        {
+            $value = $value->toDb();
+        }
+
+        $value = intval($value);
+
+        return parent::setValue($value);
     }
 
 }
