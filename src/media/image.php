afeb10a88df7de56e476e77b9fb30701e4b3026d
janionano <janionano@gmail.com>
2023-03-07 14:04:41 -0300

    fix to avoid warnings in jpegs

--- src/media/image.php
+++ src/media/image.php
@@ -163,7 +163,6 @@ class Image extends \Disk\File
             return;
         }
 
-        //$exif = @exif_read_data($this->path, 'APP1');
         $exif = @exif_read_data($this->path, 'EXIF');
 
         if (!empty($exif['Orientation']))
