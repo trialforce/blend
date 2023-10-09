360d0b08f3f44dc676451d59bc357acb087b4383
Janio Lindemann <janionano@gmail.com>
2023-10-09 10:59:37 -0300

    added forbidden character in toFile function

--- src/type/text.php
+++ src/type/text.php
@@ -188,7 +188,7 @@ class Text implements \Type\Generic, \JsonSerializable
         $this->toLower();
         $this->replace(' ', $space);
         $this->replace(array(' ', '.', ':', '/', '\\', '\'', '"', '[', ']', '=', '–'), $space);
-        $this->replace(array('?', '!', '(', ')', ',', '+', '%', '|', '*', '#', '°'), '');
+        $this->replace(array('?', '!', '(', ')', ',', '+', '%', '|', '*', '#', '°', '’'), '');
         //avoid ugly file name with -_-
         $this->replace(array('-', '_'), $space);
         //remove double space
