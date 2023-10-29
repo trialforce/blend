d39083d9270933815729373f194ca903c9cf0b1e
trialforce <trialforce@gmail.com>
2023-10-29 17:41:23 -0300

    A little correction in blend popup

--- src/view/blend/popup.php
+++ src/view/blend/popup.php
@@ -95,7 +95,7 @@ class Popup extends Div
         $this->append($this->outter);
         $this->generateTitle();
 
-        \View\View::getDom()->getBody()->append($this);
+        \View\View::getDom()->byId('content')->append($this);
     }
 
     function getTitle()
