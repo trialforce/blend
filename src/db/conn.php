e704ee0352eaeb5985dcc10a2c4e462f7a7dae47
trialforce <trialforce@gmail.com>
2024-06-12 17:01:47 -0300

    Removing commented line.

--- src/db/conn.php
+++ src/db/conn.php
@@ -408,7 +408,6 @@ class Conn
         }
 
         // Walk the array to see if we can add single-quotes to strings
-        //array_walk($values, create_function('&$v, $k', 'if (!is_numeric($v) && $v!="NULL") $v = "\'".$v."\'";'));
         array_walk($values, function (&$v, $k)
         {
             if (!is_numeric($v) && $v != "NULL")
