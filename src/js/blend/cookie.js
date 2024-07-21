6ba9f3a837aca585e323904c99d1fbf662ab125e
trialforce <trialforce@gmail.com>
2024-07-20 23:08:41 -0300

    Arrumando modo noturno para ficar ativo. Tentando mudar a cor do thema quando o app é instalado.

--- src/js/blend/cookie.js
+++ src/js/blend/cookie.js
@@ -1,20 +1,22 @@
 function setCookie(variable, value)
 {
-    var d = new Date();
+    let d = new Date();
     d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = variable + "=" + value + ";" + expires + ";path=/;SameSite=Strict;secure";
+    let expires = "expires=" + d.toUTCString();
+    //add support for localhost on http
+    let secure = window.location.protocol =='https://' ? 'secure' : '';
+    document.cookie = variable + "=" + value + ";" + expires + ";path=/;SameSite=Strict;"+secure;
 }
 
 function getCookie(variable)
 {
-    var name = variable + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
+    let name = variable + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
 
-    for (var i = 0; i < ca.length; i++)
+    for (let i = 0; i < ca.length; i++)
     {
-        var c = ca[i];
+        let c = ca[i];
 
         while (c.charAt(0) == ' ')
         {
