3b641fbf777c33ed3ac205c8cc739930e53f6150
trialforce <trialforce@gmail.com>
2025-04-24 09:57:58 -0300

    Improved log on migration manager.

--- src/db/migration/manager.php
+++ src/db/migration/manager.php
@@ -20,30 +20,62 @@ class Manager
      */
     protected $folder = '';
 
-    protected $currentVersion;
-
+    /**
+     * Construct the manager
+     * @param $connInfoId string the conninfo id where the migration table are
+     * @param $folder string the folder with migration files
+     */
     public function __construct($connInfoId = NULL, $folder = '')
     {
         $this->setConnInfoId($connInfoId);
         $this->setFolder($folder);
     }
 
+    /**
+     * Add some string to the result / out
+     * @param string $result
+     * @return $this
+     */
+    public function addResult(string $result)
+    {
+        echo $result."\r\n";
+        return $this;
+    }
+
+    /**
+     * Return the conninfoid
+     * @return string
+     */
     public function getConnInfoId()
     {
         return $this->connInfoId;
     }
 
+    /**
+     * Set the conn info id
+     * @param $connInfoId
+     * @return $this
+     */
     public function setConnInfoId($connInfoId)
     {
         $this->connInfoId = $connInfoId;
         return $this;
     }
 
+    /**
+     * Get the folder with the migration files
+     * @return string
+     */
     public function getFolder()
     {
         return $this->folder;
     }
 
+    /**
+     * Set the folder with the migration files
+     * @param $folder
+     * @return $this
+     */
     public function setFolder($folder)
     {
         $this->folder = $folder;
@@ -61,7 +93,28 @@ class Manager
     }
 
     /**
+     * Get the conn info if of the current file
      *
+     * @param \Disk\File $file
+     * @return string
+     * @throws \Exception
+     */
+    public function getCurrentConnInfoId(\Disk\File $file)
+    {
+        $explode = explode('_',$file->getPath());
+        $connInfoId = $explode[1];
+        $connInfo = \Db\Conn::getConnInfo($connInfoId,false);
+
+        if ($connInfo)
+        {
+            return $connInfoId;
+        }
+
+        throw new \Exception('Impossível encontrar connInfo para "'.$connInfoId.'"');
+    }
+
+    /**
+     * Get The catalog class of the current conn info
      * @return string
      * @throws \Exception
      */
@@ -70,17 +123,10 @@ class Manager
         return \Db\Conn::getConnInfo($this->getConnInfoId())->getCatalogClass();
     }
 
-    public function getCurrentVersion()
-    {
-        return $this->currentVersion;
-    }
-
-    public function setCurrentVersion($currentVersion)
-    {
-        $this->currentVersion = $currentVersion;
-        return $this;
-    }
-
+    /**
+     * List all version files
+     * @return array
+     */
     public function getVersionList()
     {
         $list = array();
@@ -99,26 +145,6 @@ class Manager
         sort($list);
 
         return $list;
-    }
-
-    public function execute()
-    {
-        $this->createMigrationTableIfNeeeded();
-
-        $dbVersion = $this->getDbVersion();
-        $list = $this->getVersionList();
-        $updates = 0;
-
-        foreach ($list as $version)
-        {
-            if ($dbVersion < $version)
-            {
-                $this->executeVersion($version);
-                $updates++;
-            }
-        }
-
-        return $updates;
     }
 
     /**
@@ -142,6 +168,52 @@ class Manager
         return false;
     }
 
+    /**
+     * Execute the migration files needed
+     * @return int
+     * @throws \Exception
+     */
+    public function execute()
+    {
+        $this->createMigrationTableIfNeeeded();
+
+        $dbVersion = $this->getDbVersion();
+        $list = $this->getVersionList();
+        $updates = 0;
+        $currentVersion = $list[count($list)-1];
+
+        $this->addResult('Current db version: ' . $dbVersion);
+        $this->addResult('Current file version: ' . $currentVersion);
+
+        foreach ($list as $version)
+        {
+            if ($dbVersion < $version)
+            {
+                $this->addResult('Executing version: '. $version);
+                $this->executeVersion($version);
+                $updates++;
+            }
+        }
+
+        if ($updates ==0)
+        {
+            $this->addResult('Nothing to update.');
+        }
+        else
+        {
+            $this->addResult('Update succefull!');
+        }
+
+        return $updates;
+    }
+
+    /**
+     * Execute ONE version
+     * Detects if is and PHP or SQL
+     * @param $version
+     * @return void
+     * @throws \Exception
+     */
     public function executeVersion($version)
     {
         $filePath = $this->getFolder() . '/' . $version;
@@ -159,7 +231,14 @@ class Manager
         $this->insertVersion($version);
     }
 
-    public function executeVersionPhp(\Disk\File $file)
+    /**
+     * Execute an PHP version file
+     *
+     * @param \Disk\File $file
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function executeVersionPhp(\Disk\File $file)
     {
         $className = str_replace('/', '\\', str_replace('.php', '', $file->getPath()));
 
@@ -174,24 +253,12 @@ class Manager
     }
 
     /**
+     * Execute an SQL file version
+     *
      * @param \Disk\File $file
-     * @return string
-     * @throws \Exception
+     * @return bool|null
+     * @throws \ReflectionException
      */
-    public function getCurrentConnInfoId(\Disk\File $file)
-    {
-        $explode = explode('_',$file->getPath());
-        $connInfoId = $explode[1];
-        $connInfo = \Db\Conn::getConnInfo($connInfoId,false);
-
-        if ($connInfo)
-        {
-            return $connInfoId;
-        }
-
-        throw new \Exception('Impossível encontrar connInfo para "'.$connInfoId.'"');
-    }
-
     public function executeVersionSql(\Disk\File $file)
     {
         $file->load();
@@ -219,13 +286,18 @@ class Manager
                 continue;
             }
 
+            $trimmed = str_replace(["\r","\r","\r\n"], '' ,$query);
+
             try
             {
+
+                $this->addResult("Query: " . $trimmed);
                 $result = $conn->execute($query . ';');
             }
             catch (\Exception $exception)
             {
-                $message = $exception->getMessage() . ' WRONG SQL:' . $query . ';';
+                $this->addResult("ERROR IN QUERY: " . $trimmed);
+                $message = $exception->getMessage() . ' WRONG SQL:' . $trimmed. ';';
                 \Log::setExceptionMessage($exception, $message);
                 throw $exception;
             }
@@ -234,18 +306,40 @@ class Manager
         return $result;
     }
 
+    /**
+     * Get currente db version
+     * @return string
+     * @throws \Exception
+     */
     public function getDbVersion()
     {
         $query = new \Db\QueryBuilder('migration', $this->getConnInfoId());
-        return $query->setLogId('getLastMigrationVersion')->columns('max(version) as dbVersion')->where('folder', '=', $this->getFolder())->first()->dbVersion;
+
+        return $query->setLogId('getLastMigrationVersion')
+            ->columns('max(version) as dbVersion')
+            ->where('folder', '=', $this->getFolder())
+            ->first()
+            ->dbVersion;
     }
 
+    /**
+     * Insert current version on database
+     * After version is executed
+     * @param $version
+     * @return void
+     * @throws \Exception
+     */
     private function insertVersion($version)
     {
         $sql = "INSERT INTO migration (version,folder) VALUES ('$version','$this->folder');";
         $this->getConn()->execute($sql, null, 'insertVersion');
     }
 
+    /**
+     * Create migration table if needed
+     * @return void
+     * @throws \Exception
+     */
     private function createMigrationTableIfNeeeded()
     {
         $cataalogClass = $this->getCatalogClass();
@@ -274,6 +368,15 @@ ENGINE=InnoDB
         $conn->execute($sqlCreateTable, null, 'Create table migration');
     }
 
+    /**
+     * Create the sqls needed to sync an model
+     * @todo this method is not homologed
+     *
+     * @param $modelName
+     * @param $params
+     * @return bool|null
+     * @throws \Exception
+     */
     public function diffModel($modelName, $params = null)
     {
         $tableName = $modelName::getTableName();
@@ -293,6 +396,16 @@ ENGINE=InnoDB
         return $this->diffTable($tableName, $label, $columns, $params);
     }
 
+    /**
+     * Create the sqls needed to sync an table
+     * @todo this method is not homologed
+     * @param $tableName
+     * @param $comment
+     * @param $columns
+     * @param $params
+     * @return bool|null
+     * @throws \Exception
+     */
     public function diffTable($tableName, $comment, $columns, $params)
     {
         $catalog = $this->getCatalogClass();
