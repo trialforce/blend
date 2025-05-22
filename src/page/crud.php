1d1eef828db248240eebfe7f50f15771f39f538b
Janio Lindemann <janionano@gmail.com>
2025-05-22 17:33:18 -0300

    oh, it was a function call

--- src/page/crud.php
+++ src/page/crud.php
@@ -168,7 +168,7 @@ class Crud extends \Page\Page
 
         if (!$this->model && $throw)
         {
-            throw new \UserException(('Impossível encontrar registro!'));
+            throw new \UserException(_('Impossível encontrar registro!'));
         }
 
         return $this->model;
