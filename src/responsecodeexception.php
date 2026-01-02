e96f5782574d301585045e06fc6733ee4e4e0649
trialforce <trialforce@gmail.com>
2026-01-02 13:37:49 -0300

    Little tweak

--- src/responsecodeexception.php
+++ src/responsecodeexception.php
@@ -5,5 +5,9 @@
  */
 class ResponseCodeException extends \BlendException
 {
-
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->setDefaultLog(false);
+    }
 }
